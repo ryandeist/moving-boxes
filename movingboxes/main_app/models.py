5d99bae5f29c62462736c6e054d2cbe5f5749500
Ryan Deist <ryandeist@gmail.com>
2025-03-10 16:57:37 -0400

    added Item model to fill boxes.

--- movingboxes/main_app/models.py
+++ movingboxes/main_app/models.py
@@ -19,3 +19,14 @@ class Box(models.Model):
     
     def __str__(self):
         return self.name
+    
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    quantity = models.IntegerField()
+    
+    box = models.ForeignKey(Box, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+    
